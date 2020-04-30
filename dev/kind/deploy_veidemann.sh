43027e490a67892925fccd0121c146f6fda11683
Thomas Langvann <thomas.langvann@nb.no>
2020-04-30 20:12:09 +0200

    Make sure kubectl uses a development-context in deploy_veidemann.sh

--- dev/kind/deploy_veidemann.sh
+++ dev/kind/deploy_veidemann.sh
@@ -2,4 +2,7 @@
 
 SCRIPT_DIR=$(dirname $0)
 
+# Basic safeguard to ensure that we're working in a development-context
+if ! [[ $(kubectl config current-context) = *kind* ]]; then echo "WARNING: Not a dev context, use: kubectl config use-context kind-kind ";  exit 1; fi
+
 kustomize build $SCRIPT_DIR | kubectl apply -f -
