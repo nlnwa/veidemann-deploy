b6fd4e9380b2fada4e24f210eb359d07381cdb1f
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2020-12-13 20:47:41 +0100

    dev: skip authentication with environment variable SKIP_AUTHENTICATION

--- dev/minikube/deploy_veidemann.sh
+++ dev/minikube/deploy_veidemann.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$1" = true ]; then
-  SKIP_AUTHENTICATION="\"true\""
-else
-  SKIP_AUTHENTICATION="\"false\""
-fi
-
-sed -i "s|value: \".*\"|value: $SKIP_AUTHENTICATION|" kustomization.yaml
+sed -i "s|value: .*$|value: \"${SKIP_AUTHENTICATION:-false}\"|" kustomization.yaml
 
 SCRIPT_DIR=$(dirname $0)
 
